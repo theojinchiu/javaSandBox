d6d177ed673f0dd7f7c6dc155fc6db2b0f6b73e2
thch <alanchiu3730@gmail.com>
2019-12-03 14:08:36 +0800

    remove out folder

--- src/com/theoJavaSandBox/Main.java
+++ src/com/theoJavaSandBox/Main.java
@@ -3,5 +3,6 @@ package com.theoJavaSandBox;
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is a change from git edit");
+        System.out.println("adding one line to the test Branch");
     }
 }
