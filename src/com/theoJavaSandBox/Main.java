a5b4e32389aa08f2a1082d06505f14d23e768a90
thch <alanchiu3730@gmail.com>
2019-12-03 13:27:39 +0800

    Initial commit

---
+++ src/com/theoJavaSandBox/Main.java
@@ -0,0 +1,9 @@
+package com.theoJavaSandBox;
+
+public class Main {
+    public static void main(String[] args) {
+        int x1 = 3;
+        x1 = 4;
+        System.out.println("Hello World");
+    }
+}
